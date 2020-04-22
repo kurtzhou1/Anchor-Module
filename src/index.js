ecd09b7274299ba1795a3ad3c985ce16bc3702d5
kurtzhou1 <49271972+kurtzhou1@users.noreply.github.com>
2020-04-22 21:31:00 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App  = ()=> {
+    
+    let divStyle = {
+        padding: '5px',
+        height: '500px',
+        width: '100%',
+        outline: '1px solid black',
+    }
+
+    return(
+        <div>
+            <div className='bn_cogl'>
+            <div id="season" style={divStyle}>
+            anchors:season
+            </div>
+            <div id="grouptravel" style={divStyle}>
+            anchors:grouptravel
+            </div>
+            <div id="airplantickets" style={divStyle}>
+            anchors:airplantickets
+            </div>
+            <div id="booking" style={divStyle}>
+            anchors:booking
+            </div>
+            <div id="selftour" style={divStyle}>
+            anchors:selftour
+            </div>
+            <div id="tickets" style={divStyle}>
+            anchors:tickets
+            </div>
+            <div id="specialoffer" style={divStyle}>
+            anchors:specialoffer
+            </div>
+            </div>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
